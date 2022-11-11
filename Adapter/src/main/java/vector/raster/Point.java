9262f207b74841c3cdc7a509c55018185f638a00
LuisaPurifica <rm95324@fiap.com.br>
2022-11-11 09:34:10 -0300

    adapter final part

--- Adapter/src/main/java/vector/raster/Point.java
+++ Adapter/src/main/java/vector/raster/Point.java
@@ -1,6 +1,12 @@
 package vector.raster;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
 public class Point {
 
@@ -47,7 +53,7 @@ class Line {
 
 		return end != null ? end.equals(line.end) : line.end == null;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		int result = start != null ? start.hashCode() : 0;
@@ -69,14 +75,22 @@ class VectorRectangle extends VectorObject {
 	}
 }
 
-class LineToPointAdapter extends ArrayList<Point> {
+class LineToPointAdapter implements Iterable<Point> {
 
 	private static int count = 0;
+	private static Map<Integer, List<Point>> cache = new HashMap<>();
+	private int hash;
 
 	public LineToPointAdapter(Line line) {
 
+		hash = line.hashCode();
+		if (cache.get(hash) != null)
+			return;
+
 		System.out.println(String.format("%d: Generating points for line [%d,%d]-[%d-%d] (no caching)", ++count,
 				line.start.x, line.start.y, line.end.x, line.end.y));
+
+		ArrayList<Point> points = new ArrayList<>();
 
 		int left = Math.min(line.start.x, line.end.x);
 		int right = Math.max(line.start.x, line.end.x);
@@ -87,12 +101,30 @@ class LineToPointAdapter extends ArrayList<Point> {
 
 		if (dx == 0) {
 			for (int y = top; y <= bottom; ++y) {
-				add(new Point(left, y));
+				points.add(new Point(left, y));
 			}
 		} else if (dy == 0) {
 			for (int x = left; x <= right; ++x) {
-				add(new Point(x, top));
+				points.add(new Point(x, top));
 			}
 		}
+
+		cache.put(hash, points);
 	}
+
+	@Override
+	public Iterator<Point> iterator() {
+		return cache.get(hash).iterator();
+	}
+
+	@Override
+	public void forEach(Consumer<? super Point> action) {
+		Iterable.super.forEach(action);
+	}
+
+	@Override
+	public Spliterator<Point> spliterator() {
+		return Iterable.super.spliterator();
+	}
+
 }
